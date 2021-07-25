6743a076353dbfff04343464793df4a185c2fe2d
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-25 20:30:45 +0900

    ApprovalCard 컴포넌트 만들고 card ui 적용하기

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 
 const App = () => {
 
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail author="Sam" timeAgo="Today at 4:45PM" img={faker.image.avatar()} text="Hello, nice to meet  you😄" />
       <CommentDetail author="Tim" timeAgo="Today at 7:00PM" img={faker.image.avatar()} text="Have a nice day😊" />
       <CommentDetail author="Alice" timeAgo="Today at 8:35PM" img={faker.image.avatar()} text="Nice blog post✨"/>
