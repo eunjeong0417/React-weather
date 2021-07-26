82c32234a5c766aacef6fd12da4e5581b6f111ce
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-26 15:23:57 +0900

    geolocation error message 표시하기

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-   
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position), //성공
-    (err) => console.log(err) //실패
-    );
+  constructor(props) {
+    super(props);
     
-    return <div>Latitude: </div>;
+    this.state = { lat: null, errorMessage: '' };
+    
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      }, //성공
+      err => {
+        this.setState({ errorMessage: err.message });
+      } //실패
+    );
+  }
+
+  render() {
+    
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        ErrorMessage: {this.state.errorMessage}
+      </div>
+    ); 
   }
 }
 
