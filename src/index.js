4586777e86308251215ef37d9eee1cfa2ae22ae4
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-26 13:45:25 +0900

    geolocation API로 위치 확인

--- src/index.js
+++ src/index.js
@@ -1,43 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
-
-const App = () => {
-
-  return (
-    <div className="ui container comments">
-      
-      <ApprovalCard >
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          img={faker.image.avatar()}
-          text="Hello, nice to meet you😄"
-        />
-      </ApprovalCard>
-      <ApprovalCard >
-        <CommentDetail
-          author="Tim"
-          timeAgo="Today at 7:00PM"
-          img={faker.image.avatar()}
-          text="Have a nice day😊"
-        />
-      </ApprovalCard>
-
-      <ApprovalCard >
-        <CommentDetail
-          author="Alice"
-          timeAgo="Today at 8:35PM"
-          img={faker.image.avatar()}
-          text="Nice blog post✨"
-        />
-      </ApprovalCard >
-      
-    </div>
-  );
-};
+class App extends React.Component {
+   
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position), //성공
+    (err) => console.log(err) //실패
+    );
+    
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
